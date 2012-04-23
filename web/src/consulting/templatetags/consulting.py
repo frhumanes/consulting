3e56d2aea568b08d626ae3296e2fc08cf9fdefe0
bpenya <bpenya@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-04-23 09:55:03 +0000

    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@75 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

---
+++ web/src/consulting/templatetags/consulting.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def active(request, pattern):
+    # import re
+    # if re.search(pattern, request.path):
+    #     return 'active'
+    # return ''
+
+    if pattern == request.path:
+        return 'active'
+
+    return ''
