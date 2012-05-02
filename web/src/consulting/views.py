66f65600b73479db8e9c050f4e903e6cfa6256be
bpenya <bpenya@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-05-02 06:45:24 +0000

    Add HttpResponse
    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@81 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/src/consulting/views.py
+++ web/src/consulting/views.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse
 from django.utils import simplejson
 from django.contrib.auth.models import User
 from userprofile.models import Profile
