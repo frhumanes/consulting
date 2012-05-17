42cf8570fa07732b561ad46aea7433e6f652db17
bpenya <bpenya@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-05-17 14:23:18 +0000

    Change name of views and templates
    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@136 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/src/consulting/urls.py
+++ web/src/consulting/urls.py
@@ -1,26 +1,48 @@
 # -*- encoding: utf-8 -*-
-from django.conf.urls.defaults import *
-import views
+from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import url
+from consulting import views as consulting_views
 
 
 urlpatterns = patterns('',
-    url(r'^newpatient/', views.newpatient, name='newpatient'),
-    url(r'^newappointment/(?P<id_newpatient>\d+)$', views.newappointment,
-        name='newappointment'),
-    url(r'^appointments_doctor/', views.appointments_doctor,
-        name='appointments_doctor'),
-    url(r'^searcher/', views.searcher, name='searcher'),
-    url(r'^searcher_medicine/', views.searcher_medicine,
-        name='searcher_medicine'),
-    url(r'^patient_appointments/', views.patient_appointments,
-        name='patient_appointments'),
-    url(r'^patient_management/', views.patient_management,
-        name='patient_management'),
-    url(r'^personal_data_pm/(?P<patient_id>\d+)$', views.personal_data_pm,
-        name='personal_data_pm'),
-    url(r'^details_medicine_pm/(?P<medicine_id>\d+)$',
-        views.details_medicine_pm, name='details_medicine_pm'),
-    url(r'^newtreatment_pm/', views.newtreatment_pm, name='newtreatment_pm'),
-    url(r'^remove_medication/', views.remove_medication,
-        name='remove_medication'),
+    url(r'^$', consulting_views.index, name='consulting_index'),
+
+    url(r'^newpatient/', consulting_views.newpatient,
+        name='consulting_newpatient'),
+
+    url(r'^newappointment/(?P<id_newpatient>\d+)$',
+        consulting_views.newappointment,
+        name='consulting_newappointment'),
+
+    url(r'^appointments_doctor/', consulting_views.appointments_doctor,
+        name='consulting_appointments_doctor'),
+
+    url(r'^searcher/', consulting_views.searcher,
+        name='consulting_searcher'),
+
+    url(r'^searcher_medicine/', consulting_views.searcher_medicine,
+        name='consulting_searcher_medicine'),
+
+    url(r'^patient_appointments/', consulting_views.patient_appointments,
+        name='consulting_patient_appointments'),
+
+    # PATIENT_MANAGEMENT
+    url(r'^patient_management/', consulting_views.patient_management,
+        name='consulting_index_pm'),
+
+    url(r'^personal_data_pm/(?P<patient_id>\d+)$',
+        consulting_views.personal_data_pm,
+        name='consulting_personal_data_pm'),
+
+    url(r'^list_treatments_pm/', consulting_views.list_treatments_pm,
+        name='consulting_list_treatments_pm'),
+
+    url(r'^detail_treatment_pm/', consulting_views.detail_treatment_pm,
+        name='consulting_detail_treatment_pm'),
+
+    url(r'^newtreatment_pm/', consulting_views.newtreatment_pm,
+        name='consulting_newtreatment_pm'),
+
+    url(r'^remove_medication_pm/', consulting_views.remove_medication_pm,
+        name='consulting_remove_medication_pm'),
 )
