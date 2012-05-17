0e113cd0b1a6c9973e5987088eebc2f492b95970
bpenya <bpenya@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-05-17 14:22:49 +0000

    Login using view of django
    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@135 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/src/registration/views.py
+++ web/src/registration/views.py
@@ -1,32 +1,36 @@
 # -*- encoding: utf-8 -*-
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+# from django.contrib.auth import authenticate, login
+# from django.shortcuts import render_to_response, redirect
+# from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from registration.forms import UserRegistrationForm
+# from registration.forms import UserRegistrationForm
 
 
-def login_consulting(request, form_class=UserRegistrationForm):
+# def login_consulting(request, form_class=UserRegistrationForm):
 
-    if request.method == 'POST':
-        form = form_class(data=request.POST)
-        # form.non_field_errors(in template login.html) controls
-        # if account isn't active or username or password aren't correct
-        # If username and password are correct you can carry on
+#     if request.method == 'POST':
+#         next = request.POST.get('next', 'index')
+#         form = form_class(data=request.POST)
+#         # form.non_field_errors(in template login.html) controls
+#         # if account isn't active or username or password aren't correct
+#         # If username and password are correct you can carry on
 
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            login(request, user)
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user = authenticate(username=username, password=password)
+#             login(request, user)
 
-            return HttpResponseRedirect(reverse('main_index'))
-    else:
-        form = form_class()
+#             # return HttpResponseRedirect(reverse('index'))
+#             return redirect(next)
+#     else:
+#         next = request.GET.get('next', 'index')
+#         form = form_class()
 
-    return render_to_response('registration/login.html', {'form': form},
-                                context_instance=RequestContext(request))
+#     return render_to_response('registration/login.html',
+#                                 {'form': form, 'next': next},
+#                                 context_instance=RequestContext(request))
 
 
 def logout(request):
