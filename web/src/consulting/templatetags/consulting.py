ec19036db7f2781fb55f3072165245a34826f241
bpenya <bpenya@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-05-18 12:56:48 +0000

    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@160 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/src/consulting/templatetags/consulting.py
+++ web/src/consulting/templatetags/consulting.py
@@ -4,18 +4,22 @@ register = template.Library()
 
 
 @register.simple_tag
-def active(request, pattern):
-    # import re
-    # if re.search(pattern, request.path):
-    #     return 'active'
-    # # return ''
+def active_root(request, pattern):
+    split_path = request.path.split('/')
+    split_pattern = pattern.split('/')
 
-    # print '******REQUEST.PATH******'
-    # print request.path
-    # print '******PATTERN******'
-    # print pattern
-    # print '-------------------------------------------'
-    if pattern == request.path:
+    if split_path[1] == split_pattern[1]:
+        return 'active'
+
+    return ''
+
+
+@register.simple_tag
+def active_child(request, pattern):
+    split_path = request.path.split('/')
+    split_pattern = pattern.split('/')
+
+    if split_path[1] == split_pattern[1] and split_path[2] == split_pattern[2]:
         return 'active'
 
     return ''
