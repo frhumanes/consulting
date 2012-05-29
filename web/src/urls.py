a6b0999049bc327b22beb5df7671dbc2122ee45c
ahinojo <ahinojo@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-05-29 16:03:04 +0000

    fixed last commit of urls
    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@190 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/src/urls.py
+++ web/src/urls.py
@@ -5,12 +5,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^account/', include('registration.urls')),
-    (r'^admin/', include(admin.site.urls)),
     (r'^', include('consulting.urls')),
-
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
+        name='login'),
+    # url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+    #     name='logout'),
+    (r'^accounts/', include('registration.urls')),
+    (r'^admin/', include(admin.site.urls)),
     (r'^messages/', include('private_messages.urls')),
 )
-
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-urlpatterns += staticfiles_urlpatterns()
