72cb2505fdbb7caabb7dadf39f66dec16e78ad64
ahinojo <ahinojo@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-05-31 09:10:14 +0000

    Added fixtures
    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@205 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/src/fabfile.py
+++ web/src/fabfile.py
@@ -8,10 +8,15 @@ from wtdeploy import push
 
 
 def development():
+    env.app_name = 'consulting'
+    env.fixtures_name = {
+        'medicament': ['01_group', '02_component'],
+        'consulting': ['03_category', '04_block', '05_survey', '06_question'],
+    }
     env.ip = '89.140.10.57'
     env.nginx_serves_static = True
 
-    env.deploy_folder = "/home/rambot/consulting/"
+    env.deploy_folder = "/home/rambot/consulting"
     env.hosts = ['89.140.10.57']
     env.user = 'rambot'
     env.password = 'ramboFTW'
