8c87011b3cc652ac2f6a569bbf70211824d05ef0
bpenya <bpenya@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-07-02 08:41:34 +0000

    I have created age and clean. I have modified form
    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@288 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/src/userprofile/forms.py
+++ web/src/userprofile/forms.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+from datetime import date
 from django import forms
 from django.contrib.localflavor.es.forms import ESIdentityCardNumberField
 from django.contrib.localflavor.es.forms import ESPostalCodeField
@@ -7,25 +8,10 @@ from django.utils.translation import ugettext_lazy as _
 from django.forms.widgets import DateInput
 from django.conf import settings
 from models import Profile
-from consulting.validators import validate_choice
 
 
 class ProfileForm(forms.ModelForm):
-    SEX = (
-        (-1, _(u'Seleccione el sexo')),
-        (1, _(u'Mujer')),
-        (2, _(u'Hombre')),
-    )
-    STATUS = (
-        (-1, _(u'Seleccione el estado civil')),
-        (1, _(u'Casado/a')),
-        (2, _(u'Pareja estable')),
-        (3, _(u'Divorciado/a')),
-        (4, _(u'Viudo/a')),
-        (5, _(u'Soltero/a')),
-        (6, _(u'Otros')),
-    )
-
+    ADULT = 18
     ROLE = (
         (settings.DOCTOR, _(u'Médico')),
         (settings.ADMINISTRATIVE, _(u'Administrativo')),
@@ -37,22 +23,63 @@ class ProfileForm(forms.ModelForm):
                                     max_length=150)
     second_surname = forms.CharField(label=_(u'Segundo Apellido'),
                                         max_length=150)
-    nif = ESIdentityCardNumberField(label=_(u'NIF'))
-    sex = forms.ChoiceField(label=_(u'Sexo'), choices=SEX,
-                            validators=[validate_choice])
-    address = forms.CharField(label=_(u'Dirección'), max_length=150)
-    town = forms.CharField(label=_(u'Municipio'), max_length=150)
-    postcode = ESPostalCodeField(label=_(u'Código Postal'))
-    dob = forms.DateField(label=_(u'Fecha de Nacimiento'), widget=DateInput(
-                            attrs={'class': 'span2', 'size': '16'}))
-    status = forms.ChoiceField(label=_(u'Estado Civil'), choices=STATUS,
-                                validators=[validate_choice])
+    nif = ESIdentityCardNumberField(label=_(u'NIF'), required=False,
+                    error_messages={'unique': _(u'Este NIF ya existe')})
+    sex = forms.ChoiceField(label=_(u'Sexo'), choices=Profile.SEX,
+                        required=False)
+    address = forms.CharField(label=_(u'Dirección'), max_length=150,
+                            required=False)
+    town = forms.CharField(label=_(u'Municipio'), max_length=150,
+                            required=False)
+    postcode = ESPostalCodeField(label=_(u'Código Postal'), required=False)
+    dob = forms.DateField(label=_(u'Fecha de Nacimiento'),
+                    input_formats=('%d/%m/%Y',),
+                    widget=DateInput(attrs={'class': 'span2', 'size': '16'},
+                                    format='%d/%m/%Y'),
+                    required=False)
+    status = forms.ChoiceField(label=_(u'Estado Civil'),
+                                choices=Profile.STATUS, required=False)
     phone1 = ESPhoneNumberField(label=_(u'Teléfono 1'))
     phone2 = ESPhoneNumberField(label=_(u'Teléfono 2'), required=False)
-    email = forms.EmailField(label=_(u'Correo Electrónico'), required=False)
+    email = forms.EmailField(label=_(u'Correo Electrónico'))
     profession = forms.CharField(label=_(u'Profesión'), max_length=150,
                                 required=False)
 
+    def age(self, dob):
+        today = date.today()
+        years = today.year - dob.year
+        if today.month < dob.month or\
+            today.month == dob.month and today.day < dob.day:
+            years -= 1
+        return years
+
+    def clean(self):
+        cleaned_data = super(ProfileForm, self).clean()
+        nif = cleaned_data.get("nif")
+        dob = cleaned_data.get("dob")
+
+        if not nif:
+            if dob is None:
+                msg = _(u"Este campo es obligatorio")
+                self._errors["dob"] = self.error_class([msg])
+            else:
+                if dob > date.today():
+                    msg = _(u"Fecha de Nacimiento debe ser menor que la fecha\
+                         actual")
+                    self._errors["dob"] = self.error_class([msg])
+                else:
+                    age = self.age(dob)
+                    if age >= ProfileForm.ADULT:
+                        msg = _(u"Este campo es obligatorio")
+                        self._errors["nif"] = self.error_class([msg])
+        else:
+            if not dob is None and dob > date.today():
+                msg = _(u"Fecha de Nacimiento debe ser menor que la fecha\
+                         actual")
+                self._errors["dob"] = self.error_class([msg])
+
+        return cleaned_data
+
     class Meta:
         model = Profile
-        exclude = ('user', 'role', 'doctor', 'search_field')
+        exclude = ('user', 'role', 'doctor', 'search_field', 'patients')
