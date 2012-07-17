eabf5b94a7e85c28d7117d24345dfbde592f2d3c
bpenya <bpenya@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-07-17 08:24:53 +0000

    New data model. Others illnesses
    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@323 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/src/consulting/models.py
+++ web/src/consulting/models.py
@@ -1,106 +1,121 @@
-#!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
+from django.conf import settings
 from survey.models import Option, Survey
 from medicament.models import Component
-from django.conf import settings
+from illness.models import Illness
 
 
-class Report(models.Model):
-    KIND = (
-        (1, _(u'Por fedecto')),
-        (2, _(u'Sucesivo')),
+class Task(models.Model):
+    patient = models.ForeignKey(User, related_name='patient_tasks')
+
+    survey = models.ForeignKey(Survey, related_name="survey_tasks")
+
+    self_administered = models.NullBooleanField(_(u'¿Tarea autoadministrada?'))
+
+    creation_date = models.DateTimeField(_(u'Fecha de creación de la tarea'),
+                                        auto_now_add=True)
+
+    start_date = models.DateTimeField(_(u'Fecha de comienzo de la encuesta'),
+                                        blank=True, null=True)
+
+    end_date = models.DateTimeField(_(u'Fecha de finalización de la encuesta'),
+                                    blank=True, null=True)
+
+    from_date = models.DateTimeField(
+                    _(u'Fecha a partir de la cual puede realizar la encuesta'),
+                    blank=True, null=True)
+
+    to_date = models.DateTimeField(
+                    _(u'Fecha hasta la que puede realizar la encuesta'),
+                    blank=True, null=True)
+
+    value = models.BooleanField(_(u'¿Seguir valorando la encuesta?'),
+                                default=True)
+
+    completed = models.BooleanField(
+                    _(u'¿Ha contestado todas las preguntas de la encuesta?'),
+                    default=False)
+
+    def __unicode__(self):
+        return u'%s %s %s' % (self.patient, self.survey, self.creation_date)
+
+
+class Recommendation(models.Model):
+    patient = models.ForeignKey(User, related_name='patient_recommendations')
+
+    date = models.DateTimeField(_(u'Fecha'), auto_now_add=True)
+
+    content = models.CharField(_(u'Recomendación/nes'), max_length=255,
+                                blank=True)
+
+
+class Medicine(models.Model):
+    BEFORE_AFTER_CHOICES = (
+        (settings.BEFORE, _(u'Anterior')),
+        (settings.AFTER, _(u'Posterior')),
     )
+    patient = models.ForeignKey(User, related_name='patient_medicines')
 
-    patient = models.ForeignKey(User, related_name='userreports')
-    appointment = models.ForeignKey('Appointment',
-                                    related_name='appointmentreports')
+    component = models.ForeignKey(Component,
+                              related_name='component_medicines')
 
-    kind = models.IntegerField(_(u'Tipo de Informe'), choices=KIND)
-    name = models.CharField(max_length=255)
-    #observations debe ir en appointment
-    observations = models.TextField(_(u'Observaciones'), blank=True)
-    #recommendations_treatment se debe dividir en recommendations de tipo
-    #TextField y en components de tipo ManyToManyField(Component).Ambos
-    #campos deben ir en appointment
-    #La fecha de las recomendaciones es la fecha de la cita
-    recommendations_treatment = models.TextField(
-                                _(u'Recomendaciones/Tratamiento'), blank=True)
-    date = models.DateTimeField()
+    before_after_first_appointment = models.IntegerField(
+                                        _(u'Anterior/Posterior\
+                                        Primera Cita'),
+                                        choices=BEFORE_AFTER_CHOICES,
+                                        blank=True, null=True)
+    before_after_symptom = models.IntegerField(_(u'Anterior/Posterior\
+                                    síntomas psiquiátricos'),
+                                    choices=BEFORE_AFTER_CHOICES,
+                                    blank=True, null=True)
+    months = models.IntegerField(_(u'Número de meses de toma del fármaco'),
+                                    blank=True, null=True)
+    posology = models.CharField(_(u'Posología (mg/día)'), max_length=255,
+                                    blank=True)
+    date = models.DateTimeField(_(u'Fecha'), auto_now_add=True)
+
+    def __unicode__(self):
+        return u'%s' % (self.component)
+
+    def get_before_after_symptom(self):
+        if self.before_after_symptom == settings.BEFORE:
+            before_after_symptom = _(u'Anterior')
+        else:
+            before_after_symptom = _(u'Posterior')
+
+        return before_after_symptom
+
+    def get_before_after_first_appointment(self):
+        if self.before_after_first_appointment == settings.BEFORE:
+            before_after_first_appointment = _(u'Anterior')
+        else:
+            before_after_first_appointment = _(u'Posterior')
+
+        return before_after_first_appointment
 
 
-class Appointment(models.Model):
+class Result(models.Model):
+    patient = models.ForeignKey(User, related_name='patient_results')
 
-    STATUS = (
-        (settings.UNRESOLVED, _(u'Pendiente')),
-        (settings.DONE, _(u'Realizada')),
-        (settings.NOT_DONE, _(u'No Realizada')),
-        (settings.MODIFIED, _(u'Modificada')),
-        (settings.MODIFIED_DONE, _(u'Modificada/Realizada')),
-        (settings.MODIFIED_NOT_DONE, _(u'No Realizada')),
-        (settings.MODIFIED_DELETED, _(u'Modificada/Cancelada')),
-        (settings.CANCELED, _(u'Cancelada'))
-    )
+    survey = models.ForeignKey(Survey, related_name="survey_results")
 
-    patient = models.ForeignKey(User, related_name="patientappointments")
-    doctor = models.ForeignKey(User, related_name="doctorappointments")
-    questionnaire = models.ForeignKey('Questionnaire',
-                    related_name='questionnaireappointments', blank=True,
-                    null=True)
-    # answers debe estar en questionnaire
     answers = models.ManyToManyField('Answer',
-                                    related_name="answerappointments",
-                                    blank=True, null=True)
-    treatment = models.ForeignKey('Treatment',
-                                    related_name="treatmentappointments",
-                                    blank=True, null=True)
+                                    related_name="answers_results")
 
-    status = models.IntegerField(_(u'Estado'), choices=STATUS,
-                                    blank=True, null=True)
-    date = models.DateField(_(u'Fecha de la Cita'))
-    hour = models.TimeField(_(u'Hora de la Cita'))
-    # status = models.IntegerField(_(u'Estado'), choices=STATUS, blank=True,
-    #                             null=True)
-    # date_appointment = models.DateField(_(u'Fecha de la Cita'), blank=True,
-    #                                     null=True)
-    # hour_start = models.TimeField(_(u'Hora de Inicio de la Cita'),
-    #                               blank=True,
-    #                                 null=True)
-    # hour_finish = models.TimeField(_(u'Hora de Fin de la Cita'), blank=True,
-    #                                 null=True)
-    # date_modified = models.DateTimeField(
-    #           _(u'Fecha y hora de modificación de la Cita'), blank=True,
-    #                 null=True)
-    # date_cancel = models.DateTimeField(
-    #                 _(u'Fecha y hora de cancelación de la Cita'), blank=True,
-    #                 null=True)
+    task = models.ForeignKey(Task, related_name="task_results")
+
+    date = models.DateTimeField(_(u'Fecha'), auto_now_add=True)
 
     def __unicode__(self):
-        return u'%s - %s' % (self.patient, self.questionnaire)
-
-
-class Questionnaire(models.Model):
-    survey = models.ForeignKey(Survey, related_name="questionnaires")
-
-    self_administered = models.BooleanField()
-    creation_date = models.DateTimeField()
-    start_date = models.DateTimeField(blank=True, null=True)
-    end_date = models.DateTimeField(blank=True, null=True)
-    from_date = models.DateTimeField(blank=True, null=True)
-    to_date = models.DateTimeField(blank=True, null=True)
-    rate = models.BooleanField(default=True)
-
-    completed = models.BooleanField()
-
-    def __unicode__(self):
-        return u'%s' % self.survey
+        return u'%s %s' % (self.patient, self.date)
 
 
 class Answer(models.Model):
-    option = models.ForeignKey(Option, related_name="answers")
+    option = models.ForeignKey(Option, related_name="option_answers")
 
     text = models.CharField(_(u'Texto'), max_length=255, blank=True)
 
@@ -108,41 +123,16 @@ class Answer(models.Model):
         return u'%s' % self.option
 
 
-#-----------------------------------------------------------------------------#
-#--------------------------------- Treatment ---------------------------------#
-#-----------------------------------------------------------------------------#
-class Treatment(models.Model):
-    patient = models.ForeignKey(User, related_name='patienttreatments')
-    from_appointment = models.BooleanField()
-    date = models.DateTimeField(_(u'Fecha'))
+class Report(models.Model):
+    patient = models.ForeignKey(User, related_name='patient_reports')
+
+    illness = models.ForeignKey(Illness, related_name='illness_reports')
+
+    survey = models.ForeignKey(Survey, related_name="survey_reports")
+
+    result = models.ForeignKey(Result, related_name='result_reports')
+
+    date = models.DateTimeField(_(u'Fecha del Informe'), auto_now_add=True)
 
     def __unicode__(self):
-        return u'%s' % self.date
-
-
-class Prescription(models.Model):
-    BEFORE_AFTER_CHOICES = (
-        (settings.BEFORE, _(u'Anterior')),
-        (settings.AFTER, _(u'Posterior')),
-    )
-
-    treatment = models.ForeignKey(Treatment,
-                                    related_name='treatmentprescriptions')
-    component = models.ForeignKey(Component,
-                              related_name='componentprescriptions')
-    before_after = models.IntegerField(_(u'Anterior/Posterior\
-                                    síntomas psiquiátricos'),
-                                    choices=BEFORE_AFTER_CHOICES)
-    months = models.IntegerField(_(u'Número de meses de toma del fármaco'))
-    posology = models.CharField(_(u'Posología (mg/día)'), max_length=255)
-
-    def __unicode__(self):
-            return u'%s' % self.treatment
-
-    def get_before_after(self):
-        if self.before_after == settings.BEFORE:
-            before_after = _(u'Anterior')
-        else:
-            before_after = _(u'Posterior')
-
-        return before_after
+        return u'%s %s' % (self.patient, self.survey, self.date)
