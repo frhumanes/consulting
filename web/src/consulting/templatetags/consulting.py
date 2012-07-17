a88de74887290ddb5801cb327d6e7800551c8ae4
bpenya <bpenya@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-07-17 08:20:46 +0000

    New templatetag
    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@320 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/src/consulting/templatetags/consulting.py
+++ web/src/consulting/templatetags/consulting.py
@@ -23,3 +23,16 @@ def active_child(request, pattern):
         return 'active'
 
     return ''
+
+
+@register.simple_tag
+def active_granchild(request, pattern):
+    split_path = request.path.split('/')
+    split_pattern = pattern.split('/')
+
+    if split_path[1] == split_pattern[1] and\
+        split_path[2] == split_pattern[2] and\
+        split_path[3] == split_pattern[3]:
+        return 'active'
+
+    return ''
