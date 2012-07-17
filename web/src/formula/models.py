a1ede8f1176fd127cfb70c2bf351787adc3cb958
bpenya <bpenya@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-07-17 08:19:57 +0000

    Related_name changed
    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@319 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/src/formula/models.py
+++ web/src/formula/models.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-# from survey.models import Block
 
 
 class Dimension(models.Model):
     name = models.CharField(_(u'Nombre'), max_length=40, blank=True)
+
     polynomial = models.TextField(_(u'Polinomio'))
+
     factor = models.DecimalField(u'Factor', max_digits=10, decimal_places=8)
 
     def __unicode__(self):
@@ -16,9 +16,11 @@ class Dimension(models.Model):
 
 
 class Variable(models.Model):
-    dimension = models.ForeignKey('Dimension', related_name='variables')
+    dimension = models.ForeignKey('Dimension',
+                                    related_name='dimension_variables')
 
     name = models.CharField(_(u'Nombre'), max_length=40, blank=True)
+
     code = models.CharField(_(u'Código'), max_length=40, blank=True)
 
     def __unicode__(self):
@@ -26,12 +28,13 @@ class Variable(models.Model):
 
 
 class Formula(models.Model):
-    # block = models.ForeignKey(Block, related_name='blockformulas')
-    variable = models.ForeignKey('Variable', related_name='variableformulas')
+    variable = models.ForeignKey('Variable', related_name='variable_formulas')
+
     sibling = models.ForeignKey('self', blank=True, null=True,
-                related_name='siblingformulas')
+                related_name='sibling_formulas')
 
     polynomial = models.CharField(_(u'Polinomio'), max_length=250)
+
     factor = models.DecimalField(u'Factor', max_digits=12, decimal_places=10)
 
     def __unicode__(self):
