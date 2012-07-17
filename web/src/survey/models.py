a9c8357cc7143c09a2df06efa568f16b8276ea78
bpenya <bpenya@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2012-07-17 08:19:15 +0000

    Related_name changed
    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@317 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/src/survey/models.py
+++ web/src/survey/models.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
 from django.db import models
@@ -7,9 +6,10 @@ from formula.models import Formula
 
 
 class Survey(models.Model):
-    blocks = models.ManyToManyField('Block', related_name='surveys')
+    blocks = models.ManyToManyField('Block', related_name='blocks_surveys')
 
     name = models.CharField(_(u'Nombre'), max_length=100)
+
     code = models.CharField(_(u'Código'), max_length=100)
 
     def __unicode__(self):
@@ -18,6 +18,7 @@ class Survey(models.Model):
 
 class Category(models.Model):
     name = models.CharField(_(u'Nombre'), max_length=100)
+
     code = models.CharField(_(u'Código'), max_length=15)
 
     def __unicode__(self):
@@ -32,11 +33,13 @@ class Block(models.Model):
     )
 
     categories = models.ManyToManyField('Category',
-                                        related_name='categoriesblocks')
-    formulas = models.ManyToManyField(Formula, related_name='formulasblocks')
+                                        related_name='categories_blocks')
+    formulas = models.ManyToManyField(Formula, related_name='formulas_blocks')
 
     kind = models.IntegerField(_(u'Tipo'), choices=KIND)
+
     name = models.CharField(_(u'Nombre'), max_length=100)
+
     code = models.CharField(_(u'Código'), max_length=100)
 
     def __unicode__(self):
@@ -46,9 +49,11 @@ class Block(models.Model):
 class Question(models.Model):
     #Es ManyToMany, ejemplo Pregunta B5 esta en la categoria Culpa-Abreviada y
     #en Culpa-Extensa
-    categories = models.ManyToManyField('Category', related_name='questions')
+    categories = models.ManyToManyField('Category',
+                                        related_name='categories_questions')
 
     text = models.CharField(_(u'Text'), max_length=500)
+
     code = models.CharField(_(u'Código'), max_length=10)
 
     def __unicode__(self):
@@ -61,16 +66,19 @@ class Option(models.Model):
         (1, _(u'Libre')),
     )
 
-    question = models.ForeignKey('Question', related_name="options")
+    question = models.ForeignKey('Question', related_name="question_options")
     #REPASAR RELACION: creo que es OneToMany
     #Un hijo solo puede pertenecer a un padre. Ejemplo F9.1 su padre es solo F9
     father = models.ForeignKey('self', blank=True, null=True,
-                related_name='fatheroptions')
+                related_name='father_options')
 
     kind = models.IntegerField(_(u'Tipo'), choices=KIND)
+
     code = models.CharField(_(u'Código'), max_length=10)
+
     weight = models.DecimalField(_(u'Peso'), max_digits=5, decimal_places=2,
         blank=True, null=True)
+
     text = models.CharField(_(u'Texto'), max_length=255)
 
     def __unicode__(self):
