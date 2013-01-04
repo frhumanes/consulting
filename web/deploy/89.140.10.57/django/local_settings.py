d88a62a18eee9443c539a23a57e6e5b617340a58
fruiz <fruiz@92e3e9a8-b16a-40ff-a0f0-73f7c07c1316>
2013-01-04 07:58:37 +0000

    M    web/src/templates/cal/app/notifications/new_email_message.txt M    web/deploy/README M    web/deploy/89.140.10.57/cron/tasks M    web/deploy/89.140.10.57/supervisor/supervisor M    web/deploy/89.140.10.57/django/local_settings.py A  + web/fabfile.py
    git-svn-id: https://svn.squada.com/svn/consulting30/trunk@423 92e3e9a8-b16a-40ff-a0f0-73f7c07c1316

--- web/deploy/89.140.10.57/django/local_settings.py
+++ web/deploy/89.140.10.57/django/local_settings.py
@@ -3,9 +3,9 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', #'django.db.backends.sqlite3', Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'consulting',                      # Or path to database file if using sqlite3.
-        'USER': 'consulting',                      # Not used with sqlite3.
-        'PASSWORD': 'consulting',                  # Not used with sqlite3.
+        'NAME': '%(database_name)s',                      # Or path to database file if using sqlite3.
+        'USER': '%(database_user)s',                      # Not used with sqlite3.
+        'PASSWORD': '%(database_pass)s',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
---
+++ web/fabfile.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+#
+# author: javi santana
+
+from fabric.api import env
+from wtdeploy import push, upgrade_env, update_conf
+
+def development():
+    env.app_name = 'consulting'
+    env.fixtures_name = {
+        None: [ '01_group', '02_component', '06_question',  '07_option', '03_category', '08_dimension', '09_variable', '10_formula', '04_block', '05_survey', '00_illness']
+    }
+    env.ip = '89.140.10.57'
+    env.nginx_serves_static = True
+
+    env.source_folder = 'src'
+    env.deploy_folder = "/home/rambot/consulting"
+    env.hosts = ['89.140.10.57']
+    env.user = 'wtdeploy'
+    env.password = 'Rp53*PF[;Zp<E@Qg;ANIICA4[Pmq?pkh'
+    env.local_conf_folder = "deploy/89.140.10.57"
+
+    env.repo = "https://intranet.wtelecom.es/svn/consulting30/trunk/web/src"
+    env.repo_user = 'fruiz'
+    env.repo_password = 'Wtelecom123'
+
+    env.database_admin = 'root'
+    env.database_admin_pass = 'cmglpa'
+
+    env.database_name = 'consulting'
+    env.database_user = 'consulting'
+    env.database_pass = 'consulting'
+
+    env.stats_db_name = env.database_name + "_stats"
+    env.create_stats_database = False
+    env.is_mobile = False
